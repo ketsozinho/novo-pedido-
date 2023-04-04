74b5163445d543295640f359a21384f1b09e9c81
ketsozinho <129871014+ketsozinho@users.noreply.github.com>
2023-04-04 14:38:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+		function moveSim() {
+			let sim = document.getElementById("sim");
+			let position = Math.random() * 500;
+			sim.style.left = position + "px";
+			sim.style.top = position + "px";
+		}
+		function moveNao() {
+			alert("Que pena!");
+		}
+	</script>
